3b016702d33e499ffc8514ecfbf44d3737a9cba7
marsho23 <126670877+marsho23@users.noreply.github.com>
2023-04-26 15:47:56 +0100

    Create demoTest.js

---
+++ test/demoTest.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+const checkPrime = require("../maths.js");
+
+describe("test suite", () => {
+  it("should equal 2", () => {
+    expect(1 + 1).to.equal(2);
+  });
+  // it("should not be prime",()=>{
+  //   expect(checkPrime(1).to.equal("false"));
+  // })
+});
+
+// describe("test suite", () => {
+//   it("should equal jimmy", () => {
+//     expect("").to.equal("jimmy");//   });
+// });
