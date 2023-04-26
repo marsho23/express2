f57a2e1f8fe2940ff6a4a7f9d99a2923c10e8006
marsho23 <126670877+marsho23@users.noreply.github.com>
2023-04-26 15:48:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 const express = require("express");
-require("./db");
+
 const app = express();
-// Example of middlewware
-// Parses body of requests and responses to JSON
-app.use(express.json());
+require("./db");
 
 const logger = (req, res, next) => {
   console.log("host:", req.host);
   console.log("method:", req.method);
   console.log("path:", req.path);
-  // Move to next function being called
-  // `return` keyword to stop this function
   return next();
+  //Return will stop executing anything after next()
+  console.log("oops");
 };
 
-// No path - will always run for all requests
-app.use(logger);
+//Will always run
+// app.use(logger);
 
 const catRoutes = require("./routes/cats");
+//Handling the response parsing request to json // Recieves a JS string and convert to java object
+app.use(express.json());
 
-// Prefixes request path with `/cats`
 app.use("/cats", catRoutes);
 
-app.get("/hello", (req, res) => {
-  res.send("Howdy, world!");
-});
-
-// Need 4 parameters for error handling
-// So JS knows the first one is the error
 app.use((err, req, res, next) => {
   res.status(err.status).send(err.msg);
 });
 
-// app.listen() is always last
 const server = app.listen(4494, () => {
-  console.log("server started on port", server.address().port);
+  console.log(
+    `Server started succesfully on port number${server.address().port}`
+  );
 });
+
+module.exports = server;
